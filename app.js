80a4273aac91870e3b7304ddf27aa6ccd604acf8
aidanhuang1 <aidanhuangsiqi@gmail.com>
2021-07-24 21:11:41 -0400

    commit

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://aidanhuang1:6p3aarxfAA1XUx41@cluster0.ku9mu.mongodb.net/NTST-Project?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
